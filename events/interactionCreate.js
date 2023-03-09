123f44fd3898aa5ac5c744c50cfe85613e427f37
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-03-09 23:12:56 +0100

    corrected verification

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,7 +2,7 @@
 const { SlashCommandBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle, ModalBuilder,Client, GatewayIntentBits, EmbedBuilder, ButtonBuilder } = require('discord.js');
 
 const axios = require('axios');
-
+const Sequelize = require('sequelize');
 const SCverifiedAccountDB = require('../models/SCverifiedAccountDB');
 const SCverifV2 = require('../models/SCVerifV2');
 const bot = require('../src/botMain');
@@ -21,80 +21,91 @@ const submissionRow = new ActionRowBuilder().addComponents(armoryInput);
 
 submissionModal.addComponents(submissionRow);
 
-
-
-async function verifyEmailExists(email) {
+async function verifyEmailExists(email, pass) {
+  console.log(email, pass)
+  let verifStatus = "Nothing changed"
   console.log("Verifying Email")
-  const response = await axios.post('https://www.skill-capped.com/lol/api/user/emailAvailable', { email: email })
- console.log(response)
-  console.log("Result: ", response.data.available)
-  return response.data.available
+  const response = await axios.post('https://www.skill-capped.com/lol/api/new/loginv2', { email:email, password: pass})
+  console.log(response.data.data.fsData.user)
+  if (response.data.success == false) { 
+    return(false, "Wrong email or password")
+  }
+
+  switch(response.data.data.fsData.user.role) {
+    case "SC_ROLE_PAID_USER":
+      verifStatus = (true, "User has active account")
+      break;
+    case "SC_ROLE_FREE_USER":
+      verifStatus =(false, "User has free account")
+      break;
+    case "SC_ROLE_STAFF":
+      verifStatus =(true, "This user is staff")
+      break;
+
+    case "SC_ROLE_ADMIN":
+      verifStatus =(true, "This user is admin")
+      break;
+    default:
+      console.log("Honestly dont even know how we got here")
+      break;
+  }
+  return verifStatus
 }
 
-
 async function addToSCDB(userID, email, tag, interaction) {
-  let track = true
-  await SCverifV2.create({
+  try {
+
+  
+  const [account, created ] = await SCverifV2.findOrCreate({ where:{  userEmail: email },
     userID: userID,
     userEmail: email,
     userTag: tag}).catch(err=>{
-
       switch(err.errors[0].message) {
         case "userID must be unique":
-          
-          interaction.reply({content:"You have been verified to an existing account", ephemeral:true})
-          giveRoleToUser(interaction)
-         
+          interaction.reply({content:"This account is already in use!", ephemeral:true})
           break;
         case "userEmail must be unique":
-          interaction.reply({content:"Mail is already in use!", ephemeral:true})
-          
+          interaction.reply({content:"This account is already in use!", ephemeral:true})
           break;
         default:
           interaction.reply({content:"You have not been verified due to technical reasons. Contact staff", ephemeral:true})
-         
+         break;
+      }})
+
+
+
+      console.log(created, account)
+      if(created) {
+        giveRoleToUser(interaction)
+        interaction.reply({content:"Your account has been verified!", ephemeral:true})
       }
-      track = false
-    })
-  return track
+      else if (account.userID == interaction.user.id && email == account.userEmail) {
+        giveRoleToUser(interaction)
+        interaction.reply({content: "You have been verified to an existing account", ephemeral:true})
+        return
+      }
+      else {
+        interaction.reply({content: "This account is already in use!", ephemeral:true})
+      }
+    
+    
+  } catch (err) {
+    console.log(err)
+    await interaction.reply({content:"something failed, contact staff", ephemeral:true})
+  }
   
 }
 
 async function giveRoleToUser(interaction) {
     if (interaction.guildId == "294958471953252353") { //WoW
         await interaction.member.roles.add(await interaction.guild.roles.cache.find(role => role.name == '🧨 Skill Capped Member'))
-
     }
     else {
         await interaction.member.roles.add(await interaction.guild.roles.cache.find(role => role.name == '💎・Infinity+'))
 }
 }
 
-async function performVerification(email, interaction) {
-    switch(await verifyEmailExists(email)){
-      case false:
-        try {
-          if(await addToSCDB(interaction.user.id, email, interaction.user.tag, interaction)) {
-            interaction.reply({content:"You have been verified, welcome!", ephemeral:true})
-          }
-      
-        } catch(err) {
-          console.log(err)
-          console.log("failed to add to SCDB or give role")
-          await interaction.reply({content:"You have not been verified due to technical reasons. Contact staff", ephemeral:true})
-          } 
-      break;
 
-    case "currently down":
-      const exampleEmbed = new EmbedBuilder().setImage("https://dxjql3wvvra4n.cloudfront.net/temp/announcement1.png")
-      await interaction.reply({embeds:[exampleEmbed], ephemeral:true})
-      break;
-        
-    default:
-        await interaction.reply({content:"This mail does not exist", ephemeral:true})
-        break;
-      
-}}
 const regexTemplateFullLink = /(https):\/\/(worldofwarcraft\.blizzard\.com\/[\w_-]+\/character\/(us|eu|kr|tw|cn|)\/[\w_-]+\/[\w_-]+\/)/
 
 module.exports = {
@@ -105,20 +116,47 @@ module.exports = {
 
         if(interaction.customId == "verificationmodal") {
             const email = interaction.fields.getTextInputValue("email")
-            await performVerification(email, interaction)
+            const password = interaction.fields.getTextInputValue("password")
+            switch(await verifyEmailExists(email, password)) {
+              case "Wrong email or password":
+                await interaction.reply({content:"Email or password incorrect", ephmereal:true})
+                break;
+              case "User has active account":
+                addToSCDB(interaction.user.id, email, interaction.user.tag, interaction)
+                break;
+              case "User has free account":
+                await interaction.reply({content:"You dont seem to have an active subscription", ephmereal:true})
+              break;
+              
+              case "This user is staff":
+                addToSCDB(interaction.user.id, email, interaction.user.tag, interaction)
+               
+                break;
+              case "This user is admin":
+                addToSCDB(interaction.user.id, email, interaction.user.tag, interaction)
+                
+                break;
+              default:
+                await interaction.reply({content:"something went wrong, please contact staff", ephemeral:true})
+            }
         }
         if(interaction.customId == "verificationbutton") {
-            const verificationmodal = new ModalBuilder()
-                .setCustomId('verificationmodal')
-                .setTitle('Verification Modal');
+          const verificationmodal = new ModalBuilder()
+          .setCustomId('verificationmodal')
+          .setTitle('Verification Modal');
 
-                const emailInput = new TextInputBuilder()
-                    .setCustomId('email')
-                    .setLabel("What is your email?")
-                    .setStyle(TextInputStyle.Short);
-                const verificationRow = new ActionRowBuilder().addComponents(emailInput);
-                verificationmodal.addComponents(verificationRow);
-                await interaction.showModal(verificationmodal);
+          const emailInput = new TextInputBuilder()
+              .setCustomId('email')
+              .setLabel("What is your email?")
+              .setStyle(TextInputStyle.Short);
+          const passwordInput = new TextInputBuilder()
+              .setCustomId('password')
+              .setLabel("What is your password?")
+              .setStyle(TextInputStyle.Short);
+          const emailVerifRow = new ActionRowBuilder().addComponents(emailInput)
+          const passVerifRow = new ActionRowBuilder().addComponents(passwordInput)
+          verificationmodal.addComponents(emailVerifRow, passVerifRow);
+          await interaction.showModal(verificationmodal);
         }
       if(interaction.customId == "submissionmodal") {
         
