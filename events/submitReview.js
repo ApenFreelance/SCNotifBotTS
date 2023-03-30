22b8463dcdce393d43129cd0a00fddc171f35b04
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-03-30 16:11:15 +0200

    Fixed indentation

--- events/submitReview.js
+++ events/submitReview.js
@@ -162,7 +162,7 @@ module.exports = {
       try {
 
       
-      await  interaction.editReply({content:`Thank you for requesting a free Skill Capped VoD Review.\n\nIf your submission is accepted, you will be tagged in a private channel where your review will be uploaded.`, ephemeral:true})
+      await interaction.editReply({content:`Thank you for requesting a free Skill Capped VoD Review.\n\nIf your submission is accepted, you will be tagged in a private channel where your review will be uploaded.`, ephemeral:true})
       await createWaitingForReviewMessage(interaction, wowChar, verifiedAccount, wowServer)
       } catch (err) {
         console.log("Failed when responding or creating message for review for NEW user", err)
