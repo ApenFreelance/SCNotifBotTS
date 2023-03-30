69a07170ce02f9712ba5455411a0a25666407a1e
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-03-30 20:07:12 +0200

    disabled button

--- events/closeSubmission.js
+++ events/closeSubmission.js
@@ -32,9 +32,9 @@ module.exports = {
         })
         //console.log(reviewInDB)
         await interaction.channel.permissionOverwrites.delete(reviewInDB.dataValues.userID).catch(e => console.log(e)); 
-        await interaction.reply({content:"Are you done?", components:[lastRow]})
+        //await interaction.reply({content:"Are you done?", components:[lastRow]})
 
-        
+
         await interaction.channel.edit({name: `closed-${submissionNr}`})
         //console.log(interaction.channel)
         await reviewInDB.update({
