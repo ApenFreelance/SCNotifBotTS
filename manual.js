f2b58d0fb452921927fc789e700a34619d558860
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-05-18 20:36:33 +0200

    Manual now works

--- manual.js
+++ manual.js
@@ -267,3 +267,163 @@ const wowClient = blizzard.wow.createInstance({
   locale: 'en_US', // optional
   token: '', // optional
 })
+
+parseDump("https://lol-content-dumps.s3.amazonaws.com/courses_v2/valorant/course_dump_1684120339425.json", "https://lol-content-dumps.s3.amazonaws.com/courses_v2/valorant/course_dump_1682641484587.json", "valorant")
+async function parseDump(newDumpString, oldDumpString, game){
+  let newDump = await axios.get(newDumpString)
+  let oldDump = await axios.get(oldDumpString)
+  newDump = newDump.data
+  oldDump = oldDump.data
+  
+    let updateObject = {}
+    let oldVideos = []
+    console.log("Sorting old videos")
+    Object.values(oldDump.videos).forEach(video =>{
+      oldVideos.push(video.title)
+    })
+    Object.values(newDump.videos).forEach(vid => {
+      if(!oldVideos.includes(vid.title)) {
+        //console.log(vid.title)
+        updateObject[vid.title] = {"courseTitle": null,"courseuuid":null,"videoTitle":vid.title, "videouuid": vid.uuid,"link":null,"tag": null,"tId": vid.tId}
+      }
+      
+    })
+        for(const video in updateObject) {
+          //console.log(updateObject[video].videouuid, "video")
+          for(const course in newDump.videosToCourses) {
+          
+          
+          //console.log(newDump.videosToCourses[course].chapters)
+    
+          if(newDump.videosToCourses[course].chapters[0].vids.find(item => item.uuid == updateObject[video].videouuid) != undefined ) {
+            //console.log(newDumpm.videosToCourses[course])
+            updateObject[video].courseTitle = course
+          }
+
+          
+          
+        }
+        
+        if (newDump.courses.find(item => item.title == updateObject[video].courseTitle) != undefined) {
+          //console.log(updateObject)
+          
+          updateObject[video].courseuuid = newDump.courses.find(item => item.title == updateObject[video].courseTitle).uuid
+          updateObject[video].tag = newDump.courses.find(item => item.title == updateObject[video].courseTitle).tags[0]
+/*           if(newDump.courses.find(item => item.title == updateObject[video].courseTitle).tags[0] == null) {
+              console.log("tag was null from dump")
+              updateObject[video].tag = "All Classes"
+          } else {
+          
+          //console.log(updateObject[video])
+        } */
+      }
+        updateObject[video].link = `https://www.skill-capped.com/${game}/browse3/course/${updateObject[video].videouuid}/${updateObject[video].courseuuid}`
+
+      }
+
+    
+    let interaction
+    await createEmbed(updateObject, game, interaction)
+    
+    
+  
+  }    
+  const jsonLocation = "./testGameData.json"
+  async function createEmbed(uploads, game, interaction)  {
+    
+    let breakdown = {}
+    let failed = []
+    fs.readFile(jsonLocation, "utf-8", function(err, data) {
+
+      data = JSON.parse(data)
+      Object.keys(data[game].roleDict).forEach(tags => {
+        breakdown[tags] = []
+      })
+      const videoAmountFound =Object.keys(uploads).length
+      console.log(videoAmountFound, "total videos for upload")
+      for(const video in uploads){
+        
+        switch(uploads[video].tag){
+          case "Agent":
+          case "Coaching":
+            //console.log("\n",video, "\n",Object.entries(data[game].agents).filter(([key, value])=> video.includes(key) || video.includes("Yoru")))
+            let whatAgent = Object.entries(data[game].agents).filter(([key, value])=> video.includes(key))
+            
+          if(whatAgent.length > 1 ) {
+            console.log("[TOO MANY AGENTS]", video, whatAgent)
+            failed[video] = uploads[video]
+            continue;
+          }
+            breakdown[whatAgent[0][1]].push(uploads[video])
+            break;
+          case "Maps":
+            breakdown["Maps"].push(uploads[video].videouuid)
+            break;
+          
+          default:
+            breakdown["All Videos"].push(uploads[video].videouuid)
+            break;
+
+        }
+      }
+        
+    //interaction.editReply({contents:`Will now attempt to post all videos.`, ephemeral:true})
+    const forLoop = async _ => {
+        console.log("start")
+      console.log(breakdown["Maps"])
+      let videosGettingPosted =0
+      for(const key in breakdown){
+        console.log("[",key, "]", breakdown[key].length)
+        videosGettingPosted+= breakdown[key].length
+      }
+      console.log("[ TOTAL ]", videosGettingPosted)
+      if(videoAmountFound != videosGettingPosted) {
+        console.log("[ ERROR ] Should be", videoAmountFound, "but was actually", videosGettingPosted)
+        return
+      }
+        
+        for(const tag in breakdown) {
+            console.log(breakdown[tag].length, tag)
+            if(breakdown[tag].length == 0) {
+                console.log("No videos uploaded for: ", tag)
+                continue
+            }
+            //console.log(data[game].logChannelID.toString())
+            let videoChannel = await bot.channels.fetch(data[game].roleDict[tag].channelid.toString()).catch(err => {
+                if(err.toString().startsWith("DiscordAPIError[10003]: Unknown Channel")){
+                    console.log(err)
+                    console.log("erred")
+                    //interaction.editReply({contents:`This channel does not exist in this server: ${tag} ( Set to: ${data[game].roleDict[tag].channelid.toString()})`, ephemeral:true})
+                    return
+                }
+            })
+            
+            await videoChannel.send(`<@&${data[game].roleDict[tag].id}>`)
+            breakdown[tag].forEach(video => {
+                
+            let videoEmbed = new EmbedBuilder()
+                .setTitle(video.courseTitle)  
+                .setAuthor({ name: `A new Skill-Capped video has been released!`, iconURL: "https://media.discordapp.net/attachments/991013102688555069/994302850580631622/unknown.png"})
+                .setDescription(`${video.videoTitle}\n[click here to watch](${video.link})`)
+                .setImage(`https://skillcappedzencoder.s3.amazonaws.com/${video.videouuid}/thumbnails/thumbnail_medium_${video.tId}.jpg`)
+            //.setFooter({text:"This submission is unclaimed"})
+            //.setThumbnail(data[game].roleDict[tag].img)
+            if(data[game].roleDict[tag].img == "") {
+                videoEmbed.setThumbnail(`https://media.discordapp.net/attachments/991013102688555069/994302850580631622/unknown.png`)
+            } else {
+                videoEmbed.setThumbnail(data[game].roleDict[tag].img)
+            }
+            
+            
+            videoChannel.send({embeds:[videoEmbed]})
+            
+            })
+        
+      }}
+      forLoop()
+      console.log("done")
+     
+    })
+    //await interaction.editReply({contents:`Dump upload completed`, ephemeral:true}).catch(err => console.log(err))
+    
+  }
