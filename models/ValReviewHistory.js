3558ffe9e981d5a5073b41056f00041491fda969
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-06-07 18:09:23 +0200

    Added val db

---
+++ models/ValReviewHistory.js
@@ -0,0 +1,27 @@
+const { DataTypes, Model } = require("sequelize");
+
+module.exports = class ValReviewHistory extends Model {
+    static init(sequelize) {
+        return super.init({
+         status: {type: DataTypes.STRING },
+         userEmail: { type: DataTypes.STRING},
+         userID: { type: DataTypes.STRING },
+         userTag: { type: DataTypes.STRING },
+         clipLink: {type: DataTypes.STRING },
+         AllTimeTier: {type: DataTypes.STRING},
+         CurrentTier: {type: DataTypes.STRING},
+         claimedAt: {type: DataTypes.DATE},
+         completedByID: {type: DataTypes.STRING},
+         completedByTag: {type: DataTypes.STRING},
+         completedAt: {type: DataTypes.DATE},
+         reviewLink: { type: DataTypes.STRING },
+         reviewRating: {type:DataTypes.INTEGER},
+         reviewRatingComment: {type: DataTypes.STRING}
+        }, {
+            tableName: "ValReviewHistory",
+            updatedAt:false,
+            createdAt:true,
+            sequelize
+        });
+    }
+}
