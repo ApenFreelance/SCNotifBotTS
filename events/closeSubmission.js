66b943b95aaf725ace11e4d88f04bb09f56b093e
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-07-12 12:01:52 +0200

    Added loggin and who closed ticket

--- events/closeSubmission.js
+++ events/closeSubmission.js
@@ -1,7 +1,8 @@
 const { ButtonBuilder, ActionRowBuilder } = require("discord.js");
 const { createReviewButtons } = require("../components/functions/createReviewButtons");
 const ReviewHistory = require("../models/ReviewHistory");
-
+const { updateClosedReviewDB } = require("../components/functions/databaseFunctions/updateValue");
+const { cLog } = require("../components/functions/cLog")
 
 
 
@@ -32,34 +33,29 @@ module.exports = {
             order: [['CreatedAt', 'DESC']]
         })
 
-        await interaction.channel.permissionOverwrites.delete(reviewInDB.dataValues.userID).catch(e => console.log(e)); 
+        await interaction.channel.permissionOverwrites.delete(reviewInDB.dataValues.userID).catch(e => {
+            cLog([`ERROR(${submissionNr}): `, e], {guild:interaction.guild.id, subProcess:"Remove User"})
+        }); 
         await interaction.reply({content:"Review closed!",components:[lastRow]})
         await interaction.channel.edit({name: `closed-${submissionNr}`})
-    
-        //console.log(reviewInDB)
-        
-
-
-        
-        //console.log(interaction.channel)
-        await reviewInDB.update({
-            status:"Closed"
-        })
+        cLog(["Channel updated"], {guild:interaction.guild.id, subProcess:"Close Submission"})
+        await updateClosedReviewDB(reviewInDB, interaction.user.id, interaction.user.username, interaction.guild.id, submissionNr)
         const user = await interaction.guild.members.fetch(reviewInDB.dataValues.userID)
 
         await user.send({content:"Your review has been completed.\n\n\nHow would you rate this review?", components:createReviewButtons(submissionNr, "")}).catch(err => {
             if(err.rawError.message == "Cannot send messages to this user") {
                 interaction.channel.send(`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached. Therefor channel has not been deleted.`)
+                cLog([`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached`], {guild:interaction.guild.id, subProcess:"Send Rate Request"})
+
                 return
             }
             else {
                 interaction.channel.send(`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} ), therefor channel has not been deleted.`)
+                cLog([`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} )`], {guild:interaction.guild.id, subProcess:"Send Rate Request"})
+                
                 return
             }
-            
-            })
-            
-       
-        // do your stuff
+        })
+        cLog([`Success`], {guild:interaction.guild.id, subProcess:"Send Rate Request"})
     },
 };
