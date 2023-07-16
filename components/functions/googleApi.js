42b2570a66ce177abb68f5a4452e6db65e312310
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-07-16 18:46:33 +0200

    Prettier?

--- components/functions/googleApi.js
+++ components/functions/googleApi.js
@@ -1,75 +1,37 @@
 const { google } = require("googleapis");
-const {GoogleAuth} = require('google-auth-library');
+const { GoogleAuth } = require("google-auth-library");
 async function main(data) {
   const auth = new GoogleAuth({
     keyFile: "credentials.json", //the key file
-
-    scopes: "https://www.googleapis.com/auth/spreadsheets", 
-    });
-    const service = google.sheets({version: 'v4', auth});
-
-    
-    const request = {
-      
-      spreadsheetId: '10k1pqpUUShnibEsNaRE219ICdB-Aspb0JgIrkKuvJp8', 
-  
-      resource: {
-        
-        valueInputOption: 'USER_ENTERED', 
-  
-        
-        data: data,  
-  
-        
-      },
-  
-      auth: auth,
-    };
-  
-    try {
-      const response = (await service.spreadsheets.values.batchUpdate(request)).data;
-      // TODO: Change code below to process the `response` object:
-      //console.log(JSON.stringify(response, null, 2));
-    } catch (err) {
-      console.error(err);
-    }
-  }
-async function authorize() {
-    let authClient = new google.auth.GoogleAuth({
-        keyFile: "credentials.json", //the key file
-    //url to spreadsheets API
-    scopes: "https://www.googleapis.com/auth/spreadsheets", 
-    });
-
-  
-    if (authClient == null) {
-      throw Error('authentication failed');
-    }
-  
-    return authClient;
-  }
-async function batchUpdate(spreadsheetId, title, find, replacement) {
-    
-    const {google} = require('googleapis');
-  
-    const auth = new GoogleAuth({
-      keyFile: "credentials.json", //the key file
-
-      scopes: "https://www.googleapis.com/auth/spreadsheets", 
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
-  
-    
-    try {
-      const response = await service.spreadsheets.batchUpdate({
-        spreadsheetId,
-        resource: batchUpdateRequest,
-      });
-      const findReplaceResponse = response.data.replies[1].findReplace;
-      console.log(`${findReplaceResponse.occurrencesChanged} replacements made.`);
-      return response;
-    } catch (err) {
-      
-      throw err;
-    }
+  const service = google.sheets({ version: "v4", auth });
+
+  const request = {
+    spreadsheetId: "10k1pqpUUShnibEsNaRE219ICdB-Aspb0JgIrkKuvJp8",
+    resource: {
+      valueInputOption: "USER_ENTERED",
+      data: data,
+    },
+    auth: auth,
+  };
+
+  try {
+    const response = (await service.spreadsheets.values.batchUpdate(request))
+      .data;
+  } catch (err) {
+    console.error(err);
   }
-module.exports = {main, authorize}
+}
+async function authorize() {
+  let authClient = new google.auth.GoogleAuth({
+    keyFile: "credentials.json", //the key file
+    //url to spreadsheets API
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+  if (authClient == null) {
+    throw Error("authentication failed");
+  }
+  return authClient;
+}
+module.exports = { main, authorize };
