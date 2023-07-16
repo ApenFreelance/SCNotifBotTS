fd69916c0e1e12e1478913c6b1d3b20839cf14db
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-07-16 14:46:42 +0200

    correction

--- events/closeSubmission.js
+++ events/closeSubmission.js
@@ -29,20 +29,36 @@ module.exports = {
         await interaction.reply({content:"Review closed!",components:[lastRow]})
         await interaction.channel.edit({name: `closed-${submissionNr}`})
         cLog(["Channel updated"], {guild:interaction.guild.id, subProcess:"Close Submission"})
+        if(interaction.guild.id==process.env.valServerId) {
+            await user.send({content:"Your review has been completed.\n\n\nHow would you rate this review?", components:createReviewButtons(submissionNr, "val")}).catch(err => {
+                if(err.rawError.message == "Cannot send messages to this user") {
+                    interaction.channel.send(`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached. Therefor channel has not been deleted.`)
+                    cLog([`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached`], {guild:interaction.guild.id, subProcess:"Send Rating Request"})
+                    return
+                }
+                else {
+                    interaction.channel.send(`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} ), therefor channel has not been deleted.`)
+                    cLog([`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} )`], {guild:interaction.guild.id, subProcess:"Send Rating Request"})
+                    return
+                }
+            })
+        } else if(interaction.guild.id == process.env.WoWserverId) {
+            await user.send({content:"Your review has been completed.\n\n\nHow would you rate this review?", components:createReviewButtons(submissionNr, "")}).catch(err => {
+                if(err.rawError.message == "Cannot send messages to this user") {
+                    interaction.channel.send(`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached. Therefor channel has not been deleted.`)
+                    cLog([`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached`], {guild:interaction.guild.id, subProcess:"Send Rating Request"})
+                    return
+                }
+                else {
+                    interaction.channel.send(`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} ), therefor channel has not been deleted.`)
+                    cLog([`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} )`], {guild:interaction.guild.id, subProcess:"Send Rating Request"})
+                    return
+                }
+            })
+        }
         await updateClosedReviewDB(reviewInDB, interaction.user.id, interaction.user.username, interaction.guild.id, submissionNr)
         const user = await interaction.guild.members.fetch(reviewInDB.dataValues.userID)
-        await user.send({content:"Your review has been completed.\n\n\nHow would you rate this review?", components:createReviewButtons(submissionNr, "")}).catch(err => {
-            if(err.rawError.message == "Cannot send messages to this user") {
-                interaction.channel.send(`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached. Therefor channel has not been deleted.`)
-                cLog([`${interaction.message.embeds[0].author.name} ( review-${submissionNr} ) most likely has their DM's off and could not be reached`], {guild:interaction.guild.id, subProcess:"Send Rating Request"})
-                return
-            }
-            else {
-                interaction.channel.send(`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} ), therefor channel has not been deleted.`)
-                cLog([`Unknown error when rejecting ${interaction.message.embeds[0].author.name} ( review-${submissionNr} )`], {guild:interaction.guild.id, subProcess:"Send Rating Request"})
-                return
-            }
-        })
+        
         cLog([`Success`], {guild:interaction.guild.id, subProcess:"Send Rating Request"})
     },
 };
