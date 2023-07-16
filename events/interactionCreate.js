fcce4be5968cd2903b6c408dde53b7ffbd416686
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-07-16 14:17:36 +0200

    Removing verification stuff that is no longer used

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,9 +2,6 @@
 const { SlashCommandBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle, ModalBuilder,Client, GatewayIntentBits, EmbedBuilder, ButtonBuilder } = require('discord.js');
 
 const axios = require('axios');
-const Sequelize = require('sequelize');
-const SCverifiedAccountDB = require('../models/SCverifiedAccountDB');
-const SCverifV2 = require('../models/SCVerifV2');
 const bot = require('../src/botMain');
 //const { main } = require('../components/functions/googleApi');
 const ReviewHistory = require('../models/ReviewHistory');
@@ -63,89 +60,6 @@ const trackerInput = new TextInputBuilder()
       const trackerRow = new ActionRowBuilder().addComponents(trackerInput);
 valSubmissionModal.addComponents(ytRow, trackerRow, emailRow, improvementRow)
 
-  async function verifyEmailExists(email, pass) {
-  console.log(email, pass)
-  let verifStatus = "Nothing changed"
-  console.log("Verifying Email")
-  const response = await axios.post('https://www.skill-capped.com/lol/api/new/loginv2', { email:email, password: pass})
-  console.log(response.data.data.fsData.user)
-  if (response.data.success == false) { 
-    return(false, "Wrong email or password")
-  }
-
-  switch(response.data.data.fsData.user.role) {
-    case "SC_ROLE_PAID_USER":
-      verifStatus = (true, "User has active account")
-      break;
-    case "SC_ROLE_FREE_USER":
-      verifStatus =(false, "User has free account")
-      break;
-    case "SC_ROLE_STAFF":
-      verifStatus =(true, "This user is staff")
-      break;
-
-    case "SC_ROLE_ADMIN":
-      verifStatus =(true, "This user is admin")
-      break;
-    default:
-      console.log("Honestly dont even know how we got here")
-      break;
-  }
-  return verifStatus
-}
-
-async function addToSCDB(userID, email, tag, interaction) {
-  try {
-
-  
-  const [account, created ] = await SCverifV2.findOrCreate({ where:{  userEmail: email },
-    userID: userID,
-    userEmail: email,
-    userTag: tag}).catch(err=>{
-      switch(err.errors[0].message) {
-        case "userID must be unique":
-          interaction.reply({content:"This account is already in use!", ephemeral:true})
-          break;
-        case "userEmail must be unique":
-          interaction.reply({content:"This account is already in use!", ephemeral:true})
-          break;
-        default:
-          interaction.reply({content:"You have not been verified due to technical reasons. Contact staff", ephemeral:true})
-         break;
-      }})
-
-
-
-      console.log(created, account)
-      if(created) {
-        giveRoleToUser(interaction)
-        interaction.reply({content:"Your account has been verified!", ephemeral:true})
-      }
-      else if (account.userID == interaction.user.id && email == account.userEmail) {
-        giveRoleToUser(interaction)
-        interaction.reply({content: "You have been verified to an existing account", ephemeral:true})
-        return
-      }
-      else {
-        interaction.reply({content: "This account is already in use!", ephemeral:true})
-      }
-    
-    
-  } catch (err) {
-    console.log(err)
-    await interaction.reply({content:"something failed, contact staff", ephemeral:true})
-  }
-  
-}
-
-async function giveRoleToUser(interaction) {
-    if (interaction.guildId == "294958471953252353") { //WoW
-        await interaction.member.roles.add(await interaction.guild.roles.cache.find(role => role.name == '🧨 Skill Capped Member'))
-    }
-    else {
-        await interaction.member.roles.add(await interaction.guild.roles.cache.find(role => role.name == '💎・Infinity+'))
-}
-}
 
 
 const regexTemplateFullLink = /(https):\/\/((worldofwarcraft\.blizzard\.com||worldofwarcraft\.com)\/[\w_-]+\/character\/(us|eu|kr|tw|cn|)\/[\w_-]+\/.+)/
