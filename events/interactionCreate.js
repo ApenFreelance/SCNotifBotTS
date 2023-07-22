8679e07b116fdfa095b5328587138c40e6d4d5e1
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-07-23 00:05:28 +0200

    All events now lead to same file. Still requires code within them

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,11 @@
 
 require("dotenv").config({path: '../.env'});
 const bot = require('../src/botMain');
-//const { main } = require('../components/functions/googleApi'); CHECK IF THIS IS NEEDED
+const { main } = require('../components/functions/googleApi');
 const { cLog } = require('../components/functions/cLog');
 const { createSubmissionModal } = require('../components/modals');
-const serverInfoJSON = require("../serverInfo.json")
+const serverInfoJSON = require("../serverInfo.json");
+const { getCorrectTable } = require("../src/db");
 
 
 const regexWoWLink = /(https):\/\/((worldofwarcraft\.blizzard\.com||worldofwarcraft\.com)\/[\w_-]+\/character\/(us|eu|kr|tw|cn|)\/[\w_-]+\/.+)/
@@ -19,6 +20,7 @@ module.exports = {
           await slashCommandHandler(interaction)
           return
         }
+        await interaction.reply({content:"Processing...", ephemeral:true}) // This is to show something is happening and to prevent timeout. EDIT IT ALONG THE WAY
         // End of slash command handler
         const server = selectServer(interaction.guildId) // 
         /*
@@ -28,120 +30,71 @@ module.exports = {
           reviewCategoryId
         */
 
-        if(interaction.customId == "submitreview") {
+        
+        if(interaction.customId == "submitreview") { 
+          // Show the modal for submitting. (By user)
           await createSubmissionModal(interaction, server)
         }
-
-        if(interaction.customId == "submissionmodal") {
-          await interaction.reply({content:"Processing...", ephemeral:true})
-          
+        if(interaction.customId == "submissionmodal") { 
+          // Handles response from the submitted submission through modal
           if(validLink(interaction, server.serverName)) {
-            bot.emit("submitReview", interaction)
+            // Begin submission creation handling
+            bot.emit("submitReview", interaction, server)
           }
           else {
             await interaction.editReply({content:"This link is not valid.\n\nThink this is a mistake? Let us know", ephemeral:true})
           }
         }
         if(interaction.customId == "claimsubmission") {
-          bot.emit("claimValReview", interaction)
+          // Begin claim handling
+          bot.emit("claimReview", interaction)
         }
         if(interaction.customId == "rejectsubmission") {
-          bot.emit("rejectValReview", interaction)
+          // Begin rejection handling
+          bot.emit("rejectReview", interaction)
         }
         if(interaction.customId.startsWith("closesubmission-")) {
+          // Close. NOT FINAL STEP. THIS IS WHEN REVIEW STATUS IS SET TO CLOSED. COMPLETE IS LAST
           bot.emit("closeSubmission", interaction)
         }
         if(interaction.customId.startsWith("delete-")) {
-          bot.emit("completeValReview", interaction)
+          bot.emit("completeReview", interaction)
         }
         if(interaction.customId.startsWith("completesubmission")) {
           let reviewlink = interaction.fields.getTextInputValue("reviewlink")
           cLog(["Review nr: ",interaction.customId.replace("completesubmission-", "")], {guild:interaction.guild, subProcess:"setReviewLink"})
-          const h = await ValReviewHistory.findOne({
+          
+          const reviewHistory = await getCorrectTable(server.serverId, "reviewHistory").findOne({
+            // Gets the correct table for server
             where: {
               id:interaction.customId.replace("completesubmission-", "")
             }
           })
-          await h.update({
+          await reviewHistory.update({
             reviewLink:reviewlink
           })
-          await interaction.reply({content:"Updated the review link to "+ reviewlink, ephemeral:true})
-        }
-        /// THIS NEEDS TO BE FIXED. THERE WAS A VAL CHECK BEFORE THIS
-      
-      if(/^valrating\d-\d+/.test(interaction.customId) ) {
-        bot.emit("rateValReview", interaction, "button")
-        return
-      }
-      if(interaction.customId.startsWith("valreviewratingmodal")) {
-        bot.emit("rateValReview", interaction, "modal")
-        return
-      }
-      // WOW server stuff
 
-        try {
           
-          if(interaction.customId == "claimsubmission") {
-            bot.emit("claimReview", interaction)
-          }
-          if(interaction.customId == "rejectsubmission") {
-            bot.emit("rejectReview", interaction)
-          }
-          if(interaction.customId.startsWith("completesubmission")) {
-            
-            let reviewlink = interaction.fields.getTextInputValue("reviewlink")
-            console.log(reviewlink, interaction.customId.replace("completesubmission-", ""))
 
-            const h = await ReviewHistory.findOne({
-              where: {
-                id:interaction.customId.replace("completesubmission-", "")
-              }
-            })
-            await h.update({
-              reviewLink:reviewlink
-            })
-            const forSpread = [
-              {
+          // WoW logs to sheet as well
+          if(server.serverName == "WoW") {
+            const sheetBody = [{
               "range": `T${interaction.customId.replace("completesubmission-", "")}`, //Review link
-              "values": [
-                  [
-                  reviewlink
-                  ]
-              ]
-              }
-          ]
-
-            await main(forSpread)
-            await interaction.reply({content:"Updated the review link", ephemeral:true})
+              "values": [[reviewlink]]
+            }]
+            await updateGoogleSheet(sheetBody)
           }
-          if(/^rating\d-\d+/.test(interaction.customId)) {
-            bot.emit("rateReview", interaction, "button")
-          }
-          if(interaction.customId.startsWith("delete-")) {
-            bot.emit("completeReview", interaction)
-          }
-          if(interaction.customId.startsWith("closesubmission-")) {
-            bot.emit("closeSubmission", interaction)
-          }
-          if(interaction.customId.startsWith("open-")) {
-            bot.emit("openReview", interaction)
-          }
-          if(interaction.customId.startsWith("reviewratingmodal")) {
-            bot.emit("rateReview", interaction, "modal")
-          }
-          if(interaction.customId.startsWith("clip-")) {
-            bot.emit("mediaCollection", interaction)
-          }
-        
-
-        } catch (err) {
-          if(err.toString().startsWith("TypeError: Cannot read properties of undefined (reading 'startsWith')")) {
-            console.log("Not review related")
-          } else {
-            await interaction.editReply({content:"Something went wrong, please contact staff", ephemeral:true})
-            console.log(err) }
+          await interaction.editReply({content:"Updated the review link to "+ reviewlink, ephemeral:true})
         }
-      
+
+      // WOW server stuff
+        if(interaction.customId.startsWith("clip-")) {
+          bot.emit("mediaCollection", interaction)
+        }
+        if(interaction.customId.includes("userreviewrating")) {
+          // Handle user submitted reviews to their review
+          bot.emit("rateReview", interaction)
+        }
 
           
     } catch (err) {
