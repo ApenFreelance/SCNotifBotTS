abb2912e6638c005a42e8bec0e8b9f2d5eca4b95
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-07-23 01:06:37 +0200

    Combined these two functions into one. Should work

--- components/actionRowComponents/createWaitingForReview.js
+++ components/actionRowComponents/createWaitingForReview.js
@@ -1,210 +1,125 @@
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require("discord.js");
 const bot = require("../../src/botMain");
 const classes = require("../../classes.json");
 const { cLog } = require("../functions/cLog");
+const { waitingForReviewRow } = require("../buttons");
+const { createWaitingForReviewEmbed } = require("../embeds");
 const noBreakSpace = "\u00A0";
 
-async function createWaitingForReviewMessage(
-  interaction,
-  charInfo,
-  verifiedAccount,
+async function createWaitingForReviewMessage(interaction,charInfo,
+  reviewHistory,
   improvementInput,
-  currentGuildId,
-  inputArmory,
+  linkToUserPage,
   inputName,
   channel = "1089997649245126818"
 ) {
-  const server = await bot.guilds.fetch(currentGuildId); //.name//.fetch(interaction.user.id)
   const member = await server.members.fetch(interaction.user.id);
   const submissionChannel = await bot.channels.fetch(channel);
   let description = null;
-  const maxLengt = 60;
-  if (charInfo == null) {
-    description = `
-      E-mail:\u00A0\u00A0\u00A0\u00A0\u00A0**${verifiedAccount.dataValues.userEmail}**
-      Armory:\u00A0\u00A0\u00A0\u00A0**[${inputName}](${inputArmory})**
-
-      **Failed to get data from Blizzard**
-      `;
-  } else {
-    description = `
-    E-mail:\u00A0\u00A0\u00A0\u00A0\u00A0**${verifiedAccount.dataValues.userEmail}**
-    Armory:\u00A0\u00A0\u00A0\u00A0**[${charInfo.dataValues.characterName}](${charInfo.dataValues.armoryLink})**
-    Item level:\u00A0**${charInfo.dataValues.armorLevel}**
-    Class:\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0**${charInfo.dataValues.characterClass}**
-    Region:\u00A0\u00A0\u00A0\u00A0**${charInfo.dataValues.characterRegion}**`;
-
-    if (charInfo.dataValues.twoVtwoRating != null) {
-      let n = `\n\n__2v2:${noBreakSpace.repeat()}**${
-        charInfo.dataValues.twoVtwoRating
-      }**__`.length;
-      description += `\n\n__2v2:${noBreakSpace.repeat(65 - n)}**${
-        charInfo.dataValues.twoVtwoRating
-      }**__`;
+  if(server.serverName == "WoW") {
+    if (charInfo == null) {
+      description = `E-mail:\u00A0\u00A0\u00A0\u00A0\u00A0**${reviewHistory.userEmail}**\nArmory:\u00A0\u00A0\u00A0\u00A0**[${inputName}](${linkToUserPage})**\n\n**Failed to get data from Blizzard**`;
+    } else {
+      description = `E-mail:\u00A0\u00A0\u00A0\u00A0\u00A0**${reviewHistory.userEmail}**\nArmory:\u00A0\u00A0\u00A0\u00A0**[${charInfo.characterName}](${charInfo.linkToUserPage})**\nItem level:\u00A0**${charInfo.armorLevel}**\nClass:\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0**${charInfo.characterClass}**\nRegion:\u00A0\u00A0\u00A0\u00A0**${charInfo.characterRegion}**`;
     }
-    if (charInfo.dataValues.threeVthreeRating != null) {
-      let n = `\n\n__3v3:${noBreakSpace.repeat()}**${
-        charInfo.dataValues.threeVthreeRating
-      }**__`.length;
-      description += `\n\n__3v3:${noBreakSpace.repeat(65 - n)}**${
-        charInfo.dataValues.threeVthreeRating
-      }**__`;
-    }
-    if (
-      charInfo.dataValues.soloShuffleSpec1Rating != null &&
-      charInfo.dataValues.soloShuffleSpec1Rating != undefined
-    ) {
-      let n = `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][0]
-      }:${noBreakSpace.repeat()}**${
-        charInfo.dataValues.soloShuffleSpec1Rating
-      }**__`.length;
-      description += `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][0]
-      }:${noBreakSpace.repeat(maxLengt - n)}**${
-        charInfo.dataValues.soloShuffleSpec1Rating
-      }**__`;
-    }
-    if (
-      charInfo.dataValues.soloShuffleSpec2Rating != null &&
-      charInfo.dataValues.soloShuffleSpec2Rating != undefined
-    ) {
-      let n = `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][1]
-      }:${noBreakSpace.repeat()}**${
-        charInfo.dataValues.soloShuffleSpec2Rating
-      }**__`.length;
-      description += `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][1]
-      }:${noBreakSpace.repeat(maxLengt - n)}**${
-        charInfo.dataValues.soloShuffleSpec2Rating
-      }**__`;
-    }
-    if (
-      charInfo.dataValues.soloShuffleSpec3Rating != null &&
-      charInfo.dataValues.soloShuffleSpec3Rating != undefined
-    ) {
-      let n = `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][2]
-      }:${noBreakSpace.repeat()}**${
-        charInfo.dataValues.soloShuffleSpec3Rating
-      }**__`.length;
-      description += `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][2]
-      }:${noBreakSpace.repeat(maxLengt - n)}**${
-        charInfo.dataValues.soloShuffleSpec3Rating
-      }**__`;
-    }
-    if (
-      charInfo.dataValues.soloShuffleSpec4Rating != null &&
-      charInfo.dataValues.soloShuffleSpec4Rating != undefined
-    ) {
-      let n = `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][3]
-      }:${noBreakSpace.repeat()}**${
-        charInfo.dataValues.soloShuffleSpec4Rating
-      }**__`.length;
-      description += `\n\n__Shuffle ${
-        classes[charInfo.dataValues.characterClass][3]
-      }:${noBreakSpace.repeat(maxLengt - n)}**${
-        charInfo.dataValues.soloShuffleSpec4Rating
-      }**__`;
+    description += addWoWRoleStats(charInfo, description)
+  }
+  else if(server.serverName == "Valorant") {
+    if (charInfo == null) {
+      description = `Tracker.gg:\u00A0\u00A0\u00A0\u00A0**[${inputName}](${inputTrack})**\n\n**Failed to get data from API**`;
+    } else {
+      description = `Tracker.gg:\u00A0\u00A0\u00A0\u00A0**[${charInfo.accountData.data.data.name}](${inputTrack})**\nCurrent Rank:\u00A0**${charInfo.MMRdata.data.data.current_data.currenttierpatched}**\nAll-time Rank:\u00A0**${charInfo.MMRdata.data.data.highest_rank.patched_tier}**\nElo:\u00A0\u00A0\u00A0\u00A0**${charInfo.MMRdata.data.data.current_data.elo}**`;
     }
   }
 
-  description += `\n\nClip to review: **${verifiedAccount.dataValues.clipLink}**`;
+  description += `\n\nClip to review: **${reviewHistory.clipLink}**`;
   description += `\nWhat they want to improve on: **${improvementInput}**`;
-  const waitingForReviewEmbed = new EmbedBuilder()
-    .setTitle(`Submission ${verifiedAccount.dataValues.id}`)
-    .setAuthor({
-      name: `${interaction.user.tag} ( ${interaction.user.id} )`,
-      iconURL: member.displayAvatarURL(true),
-    })
-    .setDescription(description);
-  //.setThumbnail(charInfo.characterImage)
-  //.setFooter({text:"This submission is unclaimed"})
-
-  const waitingForReviewRow = new ActionRowBuilder().addComponents(
-    new ButtonBuilder()
-      .setCustomId("claimsubmission")
-      .setLabel("Claim")
-      .setStyle("Success"),
-    new ButtonBuilder()
-      .setCustomId("rejectsubmission")
-      .setLabel("Reject")
-      .setStyle("Danger")
-  );
-  await submissionChannel.send({
-    embeds: [waitingForReviewEmbed],
-    components: [waitingForReviewRow],
-  });
+  await submissionChannel.send({embeds: [await createWaitingForReviewEmbed(interaction, reviewHistory, member, description)], components:[waitingForReviewRow]})
   cLog(["Successfully sent submission"], {
-    guild: currentGuildId,
+    guild: interaction.guildId,
     subProcess: "CreateWaitingForReview",
   });
 }
 
-async function createValWaitingForReviewMessage(
-  interaction,
-  charInfo,
-  verifiedAccount,
-  improvementInput,
-  currentGuildId,
-  inputTrack,
-  inputName,
-  channel = "1085409586997108746"
-) {
-  const server = await bot.guilds.fetch(currentGuildId); //.name//.fetch(interaction.user.id)
-  const member = await server.members.fetch(interaction.user.id);
-  const submissionChannel = await bot.channels.fetch("1084873371797434438");
-  let description = null;
+module.exports = {createWaitingForReviewMessage};
+
+
+function addWoWRoleStats(charInfo, description) {
   const maxLengt = 60;
-  if (charInfo == null) {
-    description = `
-    Tracker.gg:\u00A0\u00A0\u00A0\u00A0**[${inputName}](${inputTrack})**
-
-    **Failed to get data from API**
-    `;
-  } else {
-    description = `
-  Tracker.gg:\u00A0\u00A0\u00A0\u00A0**[${charInfo.accountData.data.data.name}](${inputTrack})**
-  Current Rank:\u00A0**${charInfo.MMRdata.data.data.current_data.currenttierpatched}**
-  All-time Rank:\u00A0**${charInfo.MMRdata.data.data.highest_rank.patched_tier}**
-  Elo:\u00A0\u00A0\u00A0\u00A0**${charInfo.MMRdata.data.data.current_data.elo}**`;
+  if (charInfo.twoVtwoRating != null) {
+    let n = `\n\n__2v2:${noBreakSpace.repeat()}**${
+      charInfo.twoVtwoRating
+    }**__`.length;
+    description += `\n\n__2v2:${noBreakSpace.repeat(65 - n)}**${
+      charInfo.twoVtwoRating
+    }**__`;
   }
-
-  description += `\n\nClip to review: **${verifiedAccount.dataValues.clipLink}**`;
-  description += `\nWhat they want to improve on: **${improvementInput}**`;
-  const waitingForReviewEmbed = new EmbedBuilder()
-    .setTitle(`Submission ${verifiedAccount.dataValues.id}`)
-    .setAuthor({
-      name: `${interaction.user.tag} ( ${interaction.user.id} )`,
-      iconURL: member.displayAvatarURL(true),
-    })
-    .setDescription(description);
-  //.setThumbnail(charInfo.characterImage)
-  //.setFooter({text:"This submission is unclaimed"})
-
-  const waitingForReviewRow = new ActionRowBuilder().addComponents(
-    new ButtonBuilder()
-      .setCustomId("claimsubmission")
-      .setLabel("Claim")
-      .setStyle("Success"),
-    new ButtonBuilder()
-      .setCustomId("rejectsubmission")
-      .setLabel("Reject")
-      .setStyle("Danger")
-  );
-  await submissionChannel.send({
-    embeds: [waitingForReviewEmbed],
-    components: [waitingForReviewRow],
-  });
-  cLog(["Successfully sent submission"], {
-    guild: currentGuildId,
-    subProcess: "CreateWaitingForReview",
-  });
+  if (charInfo.threeVthreeRating != null) {
+    let n = `\n\n__3v3:${noBreakSpace.repeat()}**${
+      charInfo.threeVthreeRating
+    }**__`.length;
+    description += `\n\n__3v3:${noBreakSpace.repeat(65 - n)}**${
+      charInfo.threeVthreeRating
+    }**__`;
+  }
+  if (
+    charInfo.soloShuffleSpec1Rating != null &&
+    charInfo.soloShuffleSpec1Rating != undefined
+  ) {
+    let n = `\n\n__Shuffle ${
+      classes[charInfo.characterClass][0]
+    }:${noBreakSpace.repeat()}**${
+      charInfo.soloShuffleSpec1Rating
+    }**__`.length;
+    description += `\n\n__Shuffle ${
+      classes[charInfo.characterClass][0]
+    }:${noBreakSpace.repeat(maxLengt - n)}**${
+      charInfo.soloShuffleSpec1Rating
+    }**__`;
+  }
+  if (
+    charInfo.soloShuffleSpec2Rating != null &&
+    charInfo.soloShuffleSpec2Rating != undefined
+  ) {
+    let n = `\n\n__Shuffle ${
+      classes[charInfo.characterClass][1]
+    }:${noBreakSpace.repeat()}**${
+      charInfo.soloShuffleSpec2Rating
+    }**__`.length;
+    description += `\n\n__Shuffle ${
+      classes[charInfo.characterClass][1]
+    }:${noBreakSpace.repeat(maxLengt - n)}**${
+      charInfo.soloShuffleSpec2Rating
+    }**__`;
+  }
+  if (
+    charInfo.soloShuffleSpec3Rating != null &&
+    charInfo.soloShuffleSpec3Rating != undefined
+  ) {
+    let n = `\n\n__Shuffle ${
+      classes[charInfo.characterClass][2]
+    }:${noBreakSpace.repeat()}**${
+      charInfo.soloShuffleSpec3Rating
+    }**__`.length;
+    description += `\n\n__Shuffle ${
+      classes[charInfo.characterClass][2]
+    }:${noBreakSpace.repeat(maxLengt - n)}**${
+      charInfo.soloShuffleSpec3Rating
+    }**__`;
+  }
+  if (
+    charInfo.soloShuffleSpec4Rating != null &&
+    charInfo.soloShuffleSpec4Rating != undefined
+  ) {
+    let n = `\n\n__Shuffle ${
+      classes[charInfo.characterClass][3]
+    }:${noBreakSpace.repeat()}**${
+      charInfo.soloShuffleSpec4Rating
+    }**__`.length;
+    description += `\n\n__Shuffle ${
+      classes[charInfo.characterClass][3]
+    }:${noBreakSpace.repeat(maxLengt - n)}**${
+      charInfo.soloShuffleSpec4Rating
+    }**__`;
+  }
+  return description
 }
-module.exports = {
-  createWaitingForReviewMessage,
-  createValWaitingForReviewMessage,
-};
