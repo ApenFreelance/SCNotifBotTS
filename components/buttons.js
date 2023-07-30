e6852d0862f8bbc6223d37ae187e678e1d173dc6
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2023-07-30 21:06:50 +0200

    standardized names

--- components/buttons.js
+++ components/buttons.js
@@ -3,23 +3,23 @@ const { ActionRowBuilder, ButtonBuilder } = require("discord.js");
 function createReviewButtons(submissionNumber, game) {
   const reviewRow = new ActionRowBuilder().addComponents(
     new ButtonBuilder()
-      .setCustomId(`${game}rating1-${submissionNumber}`)
+      .setCustomId(`${game}reviewrating1-${submissionNumber}`)
       .setLabel("1")
       .setStyle("Success"),
     new ButtonBuilder()
-      .setCustomId(`${game}rating2-${submissionNumber}`)
+      .setCustomId(`${game}reviewrating2-${submissionNumber}`)
       .setLabel("2")
       .setStyle("Success"),
     new ButtonBuilder()
-      .setCustomId(`${game}rating3-${submissionNumber}`)
+      .setCustomId(`${game}reviewrating3-${submissionNumber}`)
       .setLabel("3")
       .setStyle("Success"),
     new ButtonBuilder()
-      .setCustomId(`${game}rating4-${submissionNumber}`)
+      .setCustomId(`${game}reviewrating4-${submissionNumber}`)
       .setLabel("4")
       .setStyle("Success"),
     new ButtonBuilder()
-      .setCustomId(`${game}rating5-${submissionNumber}`)
+      .setCustomId(`${game}reviewrating5-${submissionNumber}`)
       .setLabel("5")
       .setStyle("Success")
   );
