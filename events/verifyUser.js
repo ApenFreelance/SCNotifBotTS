d33142c6e8cb2951c408764955f54fcbfe9f20d7
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-04-14 07:54:21 +0200

    Now properly gives roles if user exists

--- events/verifyUser.js
+++ events/verifyUser.js
@@ -7,17 +7,18 @@ module.exports = {
     once: false,
     async execute(interaction, server) {
         const email = interaction.fields.getTextInputValue("email")
-        cLog(["Attempting to verify  : ", interaction.user.name, email],{ guild: interaction.guild, subProcess: "VerifyUser" });
-        const [userExists, error] = verifyUserOnWebsite(email)
+        cLog(["Attempting to verify  : ", interaction.user.username, email],{ guild: interaction.guild, subProcess: "VerifyUser" });
+        const [userExists, error] = await verifyUserOnWebsite(email)
         if (error) return
 
         cLog(["Attempting to verify  : ", userExists],{ guild: interaction.guild, subProcess: "VerifyUser" });
         if (!userExists) {
-            await interaction.reply({content:`We could not find this account. Please make sure you have entered the correct email.\n\n# Believe this is a mistake?\n- [Submit Verification Request Here](${formLink})\n\nPlease be aware that it may take a few days to confirm your member status on Discord and we apologize for any delays and inconvenience.\nAdditionally, confirmations will not be accepted for YouTube screenshots. Screenshots will need to be unedited from your account page and include your email.\nPlease make sure your **DISCORD USERNAME IS CORRECT**, otherwise we will be unable to give you access or respond to your form.\n\n*Example screenshot, email hidden for privacy purposes. Screenshots **will need** to include your email to be verified.*`, files: ["../extra/images/verify_example.png"], ephemeral:true})
+            await interaction.reply({content:`We could not find this account. Please make sure you have entered the correct email.\n\n# Believe this is a mistake?\n- [Submit Verification Request Here](${formLink})\n\nPlease be aware that it may take a few days to confirm your member status on Discord and we apologize for any delays and inconvenience.\n\nAdditionally, confirmations will not be accepted for YouTube screenshots. Screenshots will need to be unedited from your account page and include your email.\n\nPlease make sure your **DISCORD USERNAME IS CORRECT**, otherwise we will be unable to give you access or respond to your form.\n\n*Example screenshot, email hidden for privacy purposes. Screenshots **will need** to include your email to be verified.*`, files: ["./extra/images/verify_example.png"], ephemeral:true})
             return
         } 
         
-
+        cLog(["Attempting to give user premium  : ", interaction.user.username],{ guild: interaction.guild, subProcess: "VerifyUser" });
+        await grantUserPremium(interaction)
     },
 };
 
@@ -52,4 +53,13 @@ async function verifyUserOnWebsite(email) {
     if(response.data.emailExistsFirebase) return [true, null]
     return [false, null]
     
+}
+const roleId = "1084871847549620285"
+async function grantUserPremium(interaction) {
+    const role = await interaction.guild.roles.fetch(roleId)
+    cLog(["Found role  : ", role.name],{ guild: interaction.guild, subProcess: "VerifyUser" });
+    
+    await interaction.member.roles.add(role)
+    await interaction.reply({content:"Your account has been succesfully linked!\nCheck out <channel> for more information about your new premium access", ephemeral: true })
+    cLog(["User granted premium  : ", interaction.user.username],{ guild: interaction.guild, subProcess: "VerifyUser" });
 }
