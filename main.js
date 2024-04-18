f4e73c43744e49c6770d7aa639763c593a601c90
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-04-18 22:48:06 +0200

    Also grabs the gear worn

--- main.js
+++ main.js
@@ -11,11 +11,48 @@ function buildParser (data) {
     const uniqueBuilds = {
         race:{},
         TalentsCodes:{},
-        pvpTalents: {}
+        pvpTalents: {},
+        gear: {
+            head:{},
+            neck:{},
+            shoulders:{},
+            back:{},
+            chest:{},
+            wrist:{},
+            hands:{},
+            waist:{},
+            legs:{},
+            feet:{},
+            rings:{},
+            trinket:{},
+            mainHand:{},
+        }
     }
     data.Characters.forEach(character => {
         uniqueBuilds.race[character.RaceSlug] = (uniqueBuilds.race[character.RaceSlug]) +1 || 1
         uniqueBuilds.TalentsCodes[character.TalentsCode] = (uniqueBuilds.TalentsCodes[character.TalentsCode] + 1) || 1
+        
+        character.Equipment.Items.forEach(item => {
+            let altSlot = null
+                if(!uniqueBuilds.gear[item.Slot]) {
+                    switch(item.Slot) {
+                        case "trinket-1":
+                        case "trinket-2":
+                            altSlot = "trinket"
+                            break;
+                        case "ring-1":
+                        case "ring-2":
+                            altSlot = "rings"
+                            break;
+
+                        case "main-hand":
+                            altSlot = "mainHand"
+                            break;
+                        default: uniqueBuilds.gear[item.Slot] = {}
+                    }
+                }
+                uniqueBuilds.gear[altSlot || item.Slot][item.Name] = uniqueBuilds.gear[altSlot || item.Slot][item.Name] +1 || 1
+        })
 
         character.PvPTalents.forEach(talent => {
             uniqueBuilds.pvpTalents[talent.Slug] = (uniqueBuilds.pvpTalents[talent.Slug]) +1 || 1
