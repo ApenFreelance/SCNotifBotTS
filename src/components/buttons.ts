08842d03fefc995f793323a1e656916040fb995a
ApenJulius <34554076+ApenJulius@users.noreply.github.com>
2024-08-09 06:20:10 +0200

    chore: Update discord.js button styles and add type annotations to button functions

--- src/components/buttons.ts
+++ src/components/buttons.ts
@@ -1,59 +1,58 @@
-import { ActionRowBuilder, ButtonBuilder } from 'discord.js'
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js'
+import { GuildIds } from '../types'
 
-function createReviewButtons(submissionNumber, game, mode = null) {
+function createReviewButtons(submissionNumber: string, game:string, mode:string | null = null) {
     const reviewRow = new ActionRowBuilder()
     const buttonAmount = 5
     for (let i = 1; i <= buttonAmount; i++) {
         const button = new ButtonBuilder()
-            .setCustomId(`${game}-reviewrating-${i}-${submissionNumber}${mode == null ? '' : '-' + mode}`)
+            .setCustomId(`${game}-reviewrating-${i}-${submissionNumber}${mode === null ? '' : '-' + mode}`)
             .setLabel(i.toString())
-            .setStyle('Success')
+            .setStyle(ButtonStyle.Success)
         reviewRow.addComponents(button)
     }
     return [reviewRow]
 }
 
-function submitReviewButton(mode) {
+function submitReviewButton(mode: string) {
     return new ActionRowBuilder().addComponents(
         new ButtonBuilder()
             .setCustomId(`submitreview-${mode}`)
             .setLabel('Submit review')
-            .setStyle('Success')
+            .setStyle(ButtonStyle.Success)
     )
 }
-function waitingForReviewRow(mode) {
+function waitingForReviewRow(mode:string) {
     return new ActionRowBuilder().addComponents(
         new ButtonBuilder()
             .setCustomId(`claimsubmission-${mode}`)
             .setLabel('Claim')
-            .setStyle('Success'),
+            .setStyle(ButtonStyle.Success),
         new ButtonBuilder()
             .setCustomId(`rejectsubmission-${mode}`)
             .setLabel('Reject')
-            .setStyle('Danger')
+            .setStyle(ButtonStyle.Danger)
     )
 }
 
-function verificationButton(guildId) {
-    if (guildId == '294958471953252353' || guildId == '1024961321768329246') { // If WoW server return these
+function verificationButton(guildId: string) { // TODO: Fix these guildIds
+    if (guildId === GuildIds.SKILLCAPPED_WOW || guildId === GuildIds.DEV) { // If WoW server return these
         return new ActionRowBuilder().addComponents(
             new ButtonBuilder()
                 .setCustomId('verify-user-wowpvp')
                 .setLabel('Verify for PVP')
-                .setStyle('Success'),
+                .setStyle(ButtonStyle.Success),
             new ButtonBuilder()
                 .setCustomId('verify-user-wowpve')
                 .setLabel('Verify for PVE')
-                .setStyle('Success'),
-            
+                .setStyle(ButtonStyle.Success),
         )
     }
-
     return new ActionRowBuilder().addComponents(
         new ButtonBuilder()
             .setCustomId('verify-user')
             .setLabel('Verify')
-            .setStyle('Success')
+            .setStyle(ButtonStyle.Success)
     )
 }
 
